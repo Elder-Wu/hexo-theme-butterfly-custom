806ef476329aa9a4288ed00a46200ba92b050579
wuming <itiswm@163.com>
2023-03-01 14:50:56 +0800

    修改自动分类脚本的bug

--- scripts/filters/auto_category.js
+++ scripts/filters/auto_category.js
@@ -1,32 +1,38 @@
-hexo.extend.filter.register('before_generate', function (data) {
+hexo.extend.filter.register('after_init', function (data) {
     const fs = require('fs');
     const path = require('path');
+
 
     function traverseDir(rootDir, category) {
         const files = fs.readdirSync(rootDir);
         files.forEach((filename) => {
             const filepath = path.join(rootDir, filename);
             if (fs.statSync(filepath).isDirectory()) {
+                // hexo.log.error(`${filename}: 是目录`)
                 // js是引用传递，递归时需要先拷贝数组，再开始递归。
                 // 否则大家都用的同一个数组，会出现问题
                 const newCategory = [...category]
-                newCategory.push(path.basename(filepath))
+                newCategory.push(filename)
                 traverseDir(filepath, newCategory);
             } else if (path.extname(filepath) === '.md') {
                 const content = fs.readFileSync(filepath, 'utf-8');
                 const regex = /categories:[\s\S]*?(?=\n[\w-]+:|\n$)/g;
                 let newContent = content.replace(regex, (match, p1, offset, string) => {
                     // 第一个文件名是"_posts","about"这些，不需要
-                    category.shift();
-                    // 把匹配到的内容替换成新的内容
-                    if (category.length == 0) {
+                    const newCategory = [...category]
+                    newCategory.shift()
+                    //把匹配到的内容替换成新的内容
+                    if (newCategory.length == 0) {
+                        hexo.log.warn(`${filename}: 无分类`);
                         match = `categories:`;
                     }
-                    if (category.length == 1) {
-                        match = `categories:\n\t- ${category.toString()}`;
+                    if (newCategory.length == 1) {
+                        hexo.log.warn(`${filename}: [${newCategory.toString()}]`);
+                        match = `categories:\n\t- ${newCategory.toString()}`;
                     }
-                    if (category.length > 1) {
-                        match = `categories:\n\t- [${category.toString()}]`;
+                    if (newCategory.length > 1) {
+                        hexo.log.warn(`${filename}: [${newCategory.toString()}]`);
+                        match = `categories:\n\t- [${newCategory.toString()}]`;
                     }
                     return match;
                 })
